6f2fe63b6eb8f0b2a7736b5d4d49a749107a4bfc
stbui <stbui@stbui.com>
2021-01-01 18:35:46 +0800

    feat: wip

--- packages/za-affix/src/index.tsx
+++ packages/za-affix/src/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
-const Root = styled.div``;
+export interface AffixProps {
+    /**
+     * 距离窗口顶部达到指定偏移量后触发
+     */
+    offsetTop?: number;
+    offset?: number;
+    /**
+     * 距离窗口底部达到指定偏移量后触发
+     */
+    offsetBottom?: number;
+    /**
+     * 固定状态改变时触发的回调函数
+     */
+    onChange?: (affixed?: boolean) => void;
+    /**
+     * 设置 Affix 需要监听其滚动事件的元素，值为一个返回对应 DOM 元素的函数
+     */
+    target?: () => Window | HTMLElement | null;
+    style?: React.CSSProperties;
+}
 
-export const Affix = props => {
-    const { ...other } = props;
+const AffixStyle = styled.div<any>``;
 
-    return <Root {...other} />;
+export const Affix: React.FC<AffixProps> = props => {
+    const { offset, ...other } = props;
+
+    const ref = useRef();
+
+    useEffect(() => {}, []);
+
+    return <AffixStyle {...other} ref={ref}></AffixStyle>;
 };
 
 Affix.defaultProps = {};
