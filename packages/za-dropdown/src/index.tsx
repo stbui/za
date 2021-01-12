de6d7ea72a5e879af3ef84f40df3fc8dd14cd526
stbui <stbui@stbui.com>
2021-01-12 18:36:44 +0800

    feat: wip

--- packages/za-dropdown/src/index.tsx
+++ packages/za-dropdown/src/index.tsx
@@ -3,12 +3,37 @@ import styled from 'styled-components';
 
 const Root = styled.div``;
 
+export interface DropDownProps {
+    trigger?: ('click' | 'hover' | 'contextMenu')[];
+    overlay: React.ReactNode;
+    onVisibleChange?: (visible?: boolean) => void;
+    visible?: boolean;
+    disabled?: boolean;
+    align?: Object;
+    getPopupContainer?: (triggerNode: HTMLElement) => HTMLElement;
+    prefixCls?: string;
+    className?: string;
+    transitionName?: string;
+    placement?:
+        | 'topLeft'
+        | 'topCenter'
+        | 'topRight'
+        | 'bottomLeft'
+        | 'bottomCenter'
+        | 'bottomRight';
+    forceRender?: boolean;
+}
+
 export const Dropdown = props => {
     const { ...other } = props;
 
     return <Root {...other} />;
 };
 
-Dropdown.defaultProps = {};
+Dropdown.defaultProps = {
+    mouseEnterDelay: 0.15,
+    mouseLeaveDelay: 0.1,
+    placement: 'bottomLeft',
+};
 
 export default Dropdown;
