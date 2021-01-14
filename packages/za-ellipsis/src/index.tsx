8df586c92105061b99eda72de39ecfc356fb1ed6
stbui <stbui@stbui.com>
2021-01-14 18:36:52 +0800

    feat: wip

--- packages/za-ellipsis/src/index.tsx
+++ packages/za-ellipsis/src/index.tsx
@@ -3,12 +3,28 @@ import styled from 'styled-components';
 
 const Root = styled.div``;
 
+interface EllipsisProps {
+    tooltip?: boolean;
+    tooltipProps?: any;
+    length?: number;
+    lines?: number;
+    fullWidthRecognition?: boolean;
+    className?: string;
+    width?: number | string;
+    style?: React.CSSProperties;
+    prefix?: string;
+}
+
 export const Ellipsis = props => {
     const { ...other } = props;
 
     return <Root {...other} />;
 };
 
-Ellipsis.defaultProps = {};
+Ellipsis.defaultProps = {
+    fullWidthRecognition: false,
+    tooltip: true,
+    tooltipProps: {},
+};
 
 export default Ellipsis;
