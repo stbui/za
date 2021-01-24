6d46bd3949106befe388d21597aae51521e4c94d
stbui <stbui@stbui.com>
2021-01-24 17:25:36 +0800

    feat(mask): update

---
+++ packages/za-mask/README.md
@@ -0,0 +1,16 @@
+# Mask
+
+## Installation
+
+```sh
+npm install @stbui/za-mask
+```
+
+## Usage
+
+```jsx
+import React from 'react';
+import Mask from '@stbui/za-mask';
+
+export default () => <Mask>content</Mask>;
+```
---
+++ packages/za-mask/src/index.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MaskRoot = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export default MaskRoot;
