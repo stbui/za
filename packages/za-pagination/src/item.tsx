8eb2d65b9edce35fe2758728050960a65ad6e818
stbui <stbui@stbui.com>
2021-09-11 18:58:55 +0800

    feat: wip

---
+++ packages/za-pagination/src/item.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+export interface PaginationItemProps {
+    active?: boolean;
+    disabled?: boolean;
+    onClick?: any;
+}
+
+export const PaginationItemStyle = styled.li<PaginationItemProps>`
+    cursor: pointer;
+    user-select: none;
+
+    list-style: none;
+    border: 1px solid #e1e3e6;
+    background-color: #fff;
+    margin-right: 8px;
+    font-family: Arial;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    min-width: 28px;
+    height: 28px;
+
+    &:hover {
+        border-color: #337eff;
+        color: #337eff;
+    }
+
+    ${({ active }) =>
+        active &&
+        css`
+            border-color: #337eff;
+            color: #337eff;
+        `}
+
+    ${({ disabled }) =>
+        disabled &&
+        css`
+            color: #999;
+            border-color: #e1e3e6;
+            cursor: not-allowed;
+
+            &:hover {
+                cursor: default;
+                color: #999;
+                border-color: #e1e3e6;
+                cursor: not-allowed;
+            }
+        `}
+`;
+
+export const PaginationItem: React.FC<PaginationItemProps> = props => {
+    const { active, children, disabled, onClick } = props;
+
+    const onLiClick = (event: React.MouseEvent) => {
+        if (disabled) return;
+
+        onClick && onClick(event);
+    };
+
+    return (
+        <PaginationItemStyle
+            onClick={onLiClick}
+            active={active}
+            disabled={disabled}
+        >
+            {children}
+        </PaginationItemStyle>
+    );
+};
+
+export default PaginationItem;
