9d0375bfa7c76de5f2cdb19c3568b6d64f8c8abc
stbui <stbui@stbui.com>
2021-09-11 16:43:20 +0800

    feat: wip

--- packages/za-badge/src/index.tsx
+++ packages/za-badge/src/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Root = styled.div``;
+const BadgeStyle = styled.span`
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    line-height: 1;
+
+    .next-badge-count {
+        color: #fff;
+        background: #f52743;
+        text-align: center;
+        white-space: nowrap;
+        border-radius: 8px;
+        position: absolute;
+        width: auto;
+        height: 16px;
+        min-width: 16px;
+        padding: 0 4px;
+        font-size: 12px;
+        line-height: 16px;
+        transform: translateX(-50%);
+        top: -0.5em;
+    }
+`;
 
 export const Badge = props => {
     const { ...other } = props;
 
-    return <Root {...other} />;
+    return (
+        <BadgeStyle>
+            <a href="#" className="basic-example"></a>
+            <sup className="badge-scroll-number badge-count">
+                <span className="badge-scroll-number-only">
+                    <span>0</span>
+                </span>
+            </sup>
+        </BadgeStyle>
+    );
 };
 
 Badge.defaultProps = {};
